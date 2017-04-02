da272557f0278c383312db8acbb056d2bda45f1e
Lyndon White <oxinabox@ucc.asn.au>
2017-04-02 18:54:27 +0800

    =Added never before saved critial sessions  type

---
+++ quick_con_program_book/sessions.py
@@ -0,0 +1,91 @@
+#CONFIG
+import pytz
+from datetime import *
+LOCAL_TIMEZONE = pytz.timezone("Australia/Perth")
+SLOT_LENGTH_MINUTES = 30
+
+DAYS = [  'Monday', 
+          'Tuesday', 
+          'Wednesday', 
+          'Thursday',  
+          'Friday', 
+          'Saturday',
+          'Sunday']
+
+
+def normalise(string): 
+    import unicodedata
+    import pylatex.utils
+    string = string.replace("\r",'')
+    string = string.replace("\xA0", "~")
+    string = string.replace("â", "~")
+    
+    while (r'\n\n' in string):
+        string=string.replace(r"\n\n",r"\n")
+        
+    
+    return string.strip()
+    
+
+def load_list(csl):
+    ldlst =  str.split(normalise(csl), ', ')
+    if ldlst==['']:
+        return []
+    else:
+        return [ll.strip() for ll in ldlst]
+    
+def parse_datetime(datetime_str):
+    if type(datetime_str) is datetime:
+        return datetime_str
+    
+    
+    import dateutil.parser
+    
+    raw = dateutil.parser.parse(datetime_str)
+    return raw.astimezone(LOCAL_TIMEZONE)
+
+
+class session(object):
+    def __init__(self, id,
+                 start_time_str,end_time_str,
+                 title,tags_str,people_str,
+                 venues_str,description):
+        self.id = int(id)
+        self.start = parse_datetime(start_time_str)
+        self.end = parse_datetime(end_time_str)
+        self.title = normalise(title)
+        self.tags = load_list(tags_str)
+        self.people = load_list(people_str)
+        self.venues = load_list(venues_str)
+        self.description = normalise(description)
+    
+    @property
+    def day(self):
+        week_days   = DAYS
+        daynum = self.start.weekday()
+        return week_days[daynum]
+    
+    
+    @property
+    def start_time(self):
+        return self.start.strftime("%H:%M")
+    
+    @property
+    def end_time(self):
+        return self.end.strftime("%H:%M")
+    
+    @property
+    def duration(self):
+        return self.end - self.start
+    
+    def __str__(self):
+        str(ss.__dict__)
+        
+def load_sessions_csv(csvpath):
+    import csv
+    sessions = []
+    with open(csvpath, 'r') as csvfile:
+        con_csv = csv.reader(csvfile)
+        next(con_csv) #Skip heading
+        sessions = [session(*row) for row in con_csv]
+    return sessions
